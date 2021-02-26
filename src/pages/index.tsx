b2ce85a226e35803bf51616115832fca1f9fdaad
Gustavo <kyoro_souza@hotmail.com>
2021-02-25 21:22:07 -0300

    🏷️ replace js for tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import { ExperienceBar } from '../components/ExperienceBar';
+
+function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default Home;
