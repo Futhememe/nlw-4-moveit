930cc555e90e7c2544c4e021ae6b9e99aaabcf73
Gustavo <kyoro_souza@hotmail.com>
2021-02-26 00:17:30 -0300

    🎨 improve home container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,29 @@
 import Head from 'next/head';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+
+import styles from '../styles/pages/Home.module.css';
 
 function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Inicio | Move.It</title>
+      </Head>
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   );
 }
