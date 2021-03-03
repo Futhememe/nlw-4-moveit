52d68fbc6742d158f8e666873fa02572525bd899
Gustavo <kyoro_souza@hotmail.com>
2021-03-03 13:21:09 -0300

    🎨 add context

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile(){
+
+  const { level } = useContext(ChallengesContext);
+
   return(
     <div className={styles.profileContainer}>
       <img src="https://github.com/Futhememe.png" alt="profile"/>
@@ -8,7 +13,7 @@ export function Profile(){
         <strong>Gustavo Souza</strong>
         <p>
           <img src="icons/level.svg" alt="up"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
